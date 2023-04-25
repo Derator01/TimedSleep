3c4143b94ff62c6be6ac53be2dfbc2b4e8778778
9thCircle <the9thcircleofhelll@gmail.com>
2023-04-25 13:13:36 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace TimedSleep;
+
+public static partial class Program
+{
+    [DllImport("Powrprof.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+    public static extern bool SetSuspendState(bool hiberate, bool forceCritical, bool disableWakeEvent);
+    private static void Main(string[] arg)
+    {
+        if (arg[0] == "a")
+        {
+            foreach (var process in Process.GetProcessesByName("TimedSleep"))
+                process.Kill();
+            return;
+        }
+
+        Thread.Sleep(int.Parse(arg[0]));
+
+        SetSuspendState(false, true, false); ;
+    }
+}
